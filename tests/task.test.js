56d0b13ac326d02855a77fae28eeba50dfeb226b
Alessandra Terra <alessandra@Fedora.lan>
2019-06-22 00:26:30 -0300

    Added more task tests.

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,15 +1,19 @@
 const request = require('supertest');
 const app = require('../src/app');
-const { testTask, testUser, setupTasks } = require('./fixtures/dbConn');
+const { testTask, testUser, testTask2, setupTasks, setupUsers } = require('./fixtures/dbConn');
+
+const token = `Bearer ${testUser.tokens[0]}`;
+const taskId = testTask._id.toString();
+const task2Id = testTask2._id.toString();
 
 beforeAll(async () => {
     await setupUsers();
     await setupTasks();
 });
 
-test('POST /tasks', async (done) => {
-    var req = request(app).post('/tasks')
-        .set("Authorization", `Bearer ${testUser.tokens[0]}`)
+test('POST /tasks success', (done) => {
+    request(app).post('/tasks')
+        .set("Authorization", token)
         .send({
             title: "test2",
             description: "test description2",
@@ -17,4 +21,63 @@ test('POST /tasks', async (done) => {
             completed: false
         })
         .expect(201, done);
-})
+});
+
+test('GET /tasks success', (done) => {
+    request(app).get('/tasks')
+        .set("Authorization", token)
+        .send()
+        .expect(200)
+        .end((err, res) => {
+            if (err) return done(err);
+            expect(res.body.length).toEqual(2); //Check if it returns only the testUser tasks.
+            done();
+        });
+});
+
+test('GET /tasks/id success', (done) => {
+    request(app).get(`/tasks/${taskId}`)
+        .set("Authorization", token)
+        .send()
+        .expect(200)
+        .end((err, res) => {
+            if (err) return done(err);
+            expect(res.body._id).toEqual(taskId);
+            done();
+        });
+});
+
+test('GET /tasks/id not found', (done) => {
+    request(app).get(`/tasks/${task2Id}`)
+        .set("Authorization", token)
+        .send()
+        .expect(404, done);
+});
+
+test('PATCH /tasks/id success', (done) => {
+    request(app).patch(`/tasks/${taskId}`)
+        .set("Authorization", token)
+        .send(testTask)
+        .expect(200, done);
+});
+
+test('PATCH /tasks/id not found', (done) => {
+    request(app).patch(`/tasks/${task2Id}`)
+        .set("Authorization", token)
+        .send(testTask)
+        .expect(404, done);
+});
+
+test('DELETE /tasks/id success', (done) => {
+    request(app).delete(`/tasks/${taskId}`)
+        .set("Authorization", token)
+        .send()
+        .expect(200, done);
+});
+
+test('DELETE /tasks/id not found', (done) => {
+    request(app).delete(`/tasks/${task2Id}`)
+        .set("Authorization", token)
+        .send()
+        .expect(404, done);
+});
